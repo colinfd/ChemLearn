758e69a4157506aa96eb068947bfc765769f0e85
Allegra Latimer <alatimer@sh-ln01.stanford.edu>
2018-11-01 19:40:48 -0700

    for testing

--- makepairs.py
+++ makepairs.py
@@ -2,12 +2,50 @@ import numpy as np
 from ase.io import read,write
 import pandas as pd
 
+gases = 'CH3 CH2 CH C NO CO OC ON NH3 NH2 NH N OH2 OH O'.split(' ')
+
 def json2atoms(json):
     f = open('temp.json','w')
     print >> f, json
     f.close()
     atoms = read('temp.json')
     return atoms
+
+def sub_atoms(self,atoms):
+    """
+    Subtract PD2 atoms from self.
+    Returns a dictionary with atom symbols as keys.
+    """
+    delta_dict = {}
+    syms1 = atoms.get_chemical_symbols()
+    syms2 = atoms.get_chemical_symbols()
+    for sym in set(syms1 + syms2):
+        delta_dict[sym] = syms1.count(sym) - syms2.count(sym)
+    return delta_dict
+
+def valid_pair(row):
+    ads_a = row['adsorbate_a']
+    ads_b = row['adsorbate_b']
+
+    atoms_a = row['atoms_a']
+    atoms_b = row['atoms_b']
+    if len(row['atoms_a']) >= len(row['atoms_b']):
+        return False
+    
+    if ads_a == 's':
+        if ads_b in gases:
+            return True
+        else:
+            return False
+    else:
+        if ads_a[0] == ads_b[0]:
+            delta_comp = atoms_b.sub(atoms_a)
+            s=0
+            for key in delta_comp :
+                s +=delta_comp[key]
+            if s==1:
+                return True
+
 
 if __name__=='__main__':
     
@@ -20,9 +58,14 @@ if __name__=='__main__':
     key = ['composition','bulk_structure','facet','cell_size']
     df.set_index(key)
     jdf = df.join(df.set_index(key),lsuffix='_a',rsuffix='_b',on=key)
+    for i in range(10):
+        jdf.drop(['moment_%s_b'%(i)],axis=1,inplace=True)
     
     #filtering
-    #jdf = jdf.ix[len(jdf['atoms_a']) >= len(jdf['atoms_a']) ]
+   #jdf = jdf.ix[jdf['atoms_a'].apply(len) < jdf['atoms_b'].apply(len) ]
+
+    jdf = jdf[jdf.apply(valid_pair,axis=1)]
+
 
 
 
