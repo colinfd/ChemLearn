0852ad11c4a5700d6324d0662f8f266d43860b68
Colin Dickens <colin.dickens28@gmail.com>
2018-12-10 16:35:07 -0800

    hacked feature_imp away to make the importance bar chart/pdos combined figure

--- feature_imp.py
+++ feature_imp.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import GroupKFold
 from skopt import BayesSearchCV
 from scipy.stats import randint as sp_randint
+from matplotlib.patches import Rectangle
 
 def evaluate(model, test_features, test_labels):
     predictions = model.predict(test_features)
@@ -25,6 +26,7 @@ models = ['rf']#['lr','rf']
 
 fig = plt.figure(figsize=(8,6))
 ax = fig.add_subplot(111)
+
 
 for feature in features:
     for m in models:
@@ -56,15 +58,39 @@ for feature in features:
         elif m == 'lr':
             model = linear_model.LinearRegression()
 
+        #ax2.plot(X_train.mean(axis=0),lw=.1)
+        ax.set_ylim((0,7))
+        #ax.fill_between(range(X_train.shape[1]),[0]*X_train.shape[1],X_train.mean(axis=0),facecolor='deepskyblue',edgecolor=None)
+        ax.fill_between(range(200),[0]*200,X_train.mean(axis=0)[:200],facecolor='deepskyblue',edgecolor='none')
+        ax.fill_between(range(352,552),[0]*200,X_train.mean(axis=0)[352:552],facecolor='darkgoldenrod',edgecolor='none')
+
+        ax.fill_between(range(199,352),[0]*(352-199),X_train.mean(axis=0)[199:352],facecolor='deepskyblue',edgecolor='none',alpha=0.3)
+        ax.fill_between(range(351,X_train.shape[1]-2),[0]*(X_train.shape[1]-2-351),X_train.mean(axis=0)[351:-2],facecolor='darkgoldenrod',edgecolor='none',alpha=0.5)
+
+        #ax.plot(X_train.mean(axis=0)[:352],'-k',lw=0.5)
+        #ax.plot(range(352,X_train.shape[1]-2),X_train.mean(axis=0)[352:X_train.shape[1]-2],'-k',lw=0.5)
+
+
         num_examples = X_train.shape[0]
         
         model.fit(X_train, y_train)
         mae_dev = evaluate(model, X_dev, y_dev)
         mae_train = evaluate(model, X_train, y_train)
-
-        ax.axvline(200,ls='--',c='r',alpha=0.5)
-        ax.axvline(552,ls='--',c='r',alpha=0.5)
-        ax.bar(range(len(model.feature_importances_)),model.feature_importances_)
+        
+        ylim = [0,0.04]
+        ax2 = ax.twinx()
+        #ax2.add_patch(Rectangle((0,0),352,0.04,fc='darkgoldenrod',ec=None,alpha=0.3))
+        #ax2.add_patch(Rectangle((352,0),352,0.04,fc='darksalmon',ec=None,alpha=0.3))
+        #ax2.plot([200]*2,ylim,'--k',lw=1)
+        #ax2.plot([552]*2,ylim,'--k',lw=1)
+        for i in range(len(model.feature_importances_[:-2])):
+            ax2.plot([i]*2,[0,model.feature_importances_[i]],'-k',lw=1)
+        
+        ax2.add_patch(Rectangle((352*2,0),10,model.feature_importances_[-2],fc='deepskyblue',ec='white'))#,alpha=0.3))
+        ax2.add_patch(Rectangle((352*2+10,0),10,model.feature_importances_[-1],fc='darkgoldenrod',ec='white'))#,alpha=0.3))
+        #ax.bar(range(len(model.feature_importances_)),model.feature_importances_)
+        ax2.set_ylim(ylim)
+        ax.set_xlim([0,352*2 + 50])
         
         #plt.bar(range(len(model.feature_importances_)),model.feature_importances_)
 
@@ -80,12 +106,12 @@ for feature in features:
 
 #plt.clf()
 
-if feature == 'pdos':
-    #for i in range(20):
-    ax2 = ax.twinx()
-    ax2.plot(X_train.mean(axis=0),lw=.1)
-    ax2.set_ylim((0,7))
-    #plt.plot(X_train.mean(axis=0))
 
-    plt.savefig('feature_importance/X1_%s.pdf'%(feature))
+for axi in [ax,ax2]:
+    axi.tick_params(axis='both',which='both',bottom=False,top=False,left=False,right=False,
+            labelbottom=False,labelright=False,labelleft=False)
 
+#ax.set_xlabel('Feature',family='sans-serif')
+
+plt.savefig('feature_importance/X1_%s.png'%(feature),dpi=300)
+#plt.show()
