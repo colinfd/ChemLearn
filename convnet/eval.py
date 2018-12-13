4c9157bc8e485b7f15bfdebd5aa6f4876d219846
Allegra Latimer <allegralatimer@MacBook-Pro-28.local>
2018-12-12 16:40:43 -0800

    cleaned up

--- convnet/eval.py
+++ convnet/eval.py
@@ -1,12 +1,59 @@
 import torch
+from dataset import PdosDataset
+import numpy as np
+import matplotlib.pyplot as plt
+import PlotFormat 
 
-split_type = 'comp_rxn'
+colors=PlotFormat.muted_colors
+
+def parity(y_train,train_preds,y_test,test_preds,model,features):
+    
+    plt.figure(figsize=(3.5,3))
+    train_mae = np.abs(y_train-train_preds).mean()
+    test_mae = np.abs(y_test-test_preds).mean()
+    print('Test MAE: %4.2f'%(test_mae))
+    print('Train MAE: %4.2f'%(train_mae))
+    plt.plot(y_train,train_preds,'.',c='grey',ms=3,label='Train MAE: %4.2f'%train_mae,alpha=1)
+    plt.plot(y_test,test_preds,'.',c=colors[model],ms=3,label='Test MAE: %4.2f'%test_mae,alpha=1)
+
+    xlim = plt.gca().get_xlim()
+    ylim = plt.gca().get_ylim()
+    
+    alim = (min(xlim[0],ylim[0]),max(xlim[1],ylim[1]))
+
+    plt.plot(alim,alim,'-k',lw=1)
+    plt.gca().set_ylim(alim)
+    plt.gca().set_xlim(alim)
+
+    plt.xlabel('$\Delta E$ (eV)')
+    plt.ylabel('$\Delta E$ Predicted (eV)')
+
+    plt.legend(loc='best')
+
+    plt.tight_layout()
+    plt.savefig('../parity/'+model+'_'+features+'_'+split_type+'_parity.pdf')
+
+#Change train test split 
+split_type = 'comp_rxn'#comp_rxn,random,comp or rxn
 
 test_dataset = PdosDataset(data_subset = 'test')
-model = torch.load('%s_model.pt'%split_type)
+train_dataset = PdosDataset(data_subset = 'train')
+model = torch.load('model_%s.pt'%split_type)
 model.eval()
 
-y_pred = model(test_dataset.X_data)
-MAE = np.abs(y_pred - test_dataset.y_data).mean()
+X_test, y_test = test_dataset[range(test_dataset.X_data.shape[0])]
+X_train, y_train = train_dataset[range(train_dataset.X_data.shape[0])]
 
-print "Test error for %s split = %.3f"%(split_type,MAE)
+y_pred,asdf,asdf = model(X_test)
+y_train_pred,asdf,asdf = model(X_train)
+MAE = np.abs(y_pred.detach().numpy()[:,0]-y_test.detach().numpy()[:]).mean()
+
+print("Test error for %s split = %.3f"%(split_type,MAE))
+
+#Make parity plot for comp_rxn only, could change
+if split_type=='comp_rxn':
+    y_train = y_train.detach().numpy()[0,:]
+    y_pred = y_pred.detach().numpy()[:,0]
+    y_train_pred = y_train_pred.detach().numpy()[:,0]
+    y_test = y_test.detach().numpy()[0,:]
+    parity(y_train,y_train_pred,y_test,y_pred,'cnn','pdos')
--- plot_lc.py
+++ plot_lc.py
@@ -8,31 +8,49 @@ from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import GroupKFold
 from skopt import BayesSearchCV
 from scipy.stats import randint as sp_randint
+import PlotFormat
 
-features = ['moments']#,'pdos']# #pdos,'moments'
-models_labels = ['Linear Regression','Random Forest','Boosting','Kernel RR']
-models = ['lr','rf','boost','krr']
+"""
+Plots learning curves for all models, need to specify 
+whether looking at moments or pdos as features
+"""
 
-colors = cm.rainbow(np.linspace(0,1,6))
+features = 'moments'
+
+if features == 'moments':
+    models_labels = ['Linear Regression','Random Forest','Gradient Boosting','Kernel Ridge Regression']
+    models = ['lr','rf','boost','krr']
+elif features == 'pdos':
+    models_labels = ['Random Forest','Conv Neural Net','Gradient Boosting']
+    models = ['rf','cnn','boost']
+
+colors = PlotFormat.muted_colors
 i = 0
 
-percent = range(10,101,10)
+fig = plt.figure(figsize=(5,4))
+ax = fig.add_subplot(111)
 
-for feature in features:
-    for m_lab,m in zip(models_labels,models):
-        i+=1
-        if (m == 'lr' or m == 'boost' )and feature == 'pdos':
-            continue
-             
-        mae_train = np.load('learning_curve/mae_train_%s_%s.npy'%(m, feature))
-        mae_dev = np.load('learning_curve/mae_dev_%s_%s.npy'%(m, feature))
-        
-        plt.plot(percent,mae_dev,'--',label=m_lab+' Dev',color=colors[i])
-        plt.plot(percent,mae_train,'-',label=m_lab+' Train',color=colors[i])
+num_train_ex = 1408
+percent = np.arange(.1,1.01,.1)
+size_train = percent*num_train_ex
 
-    plt.title(feature)
-plt.legend()
+for m_lab,m in zip(models_labels,models):
+    i+=1
+    if m == 'lr' and features == 'pdos':
+        continue
+         
+    mae_train = np.load('learning_curve/mae_train_%s_%s.npy'%(m, features))
+    mae_dev = np.load('learning_curve/mae_dev_%s_%s.npy'%(m, features))
+    
+    print(mae_dev.shape,mae_train.shape,m_lab)
+    plt.plot(size_train,mae_dev,'--',label=m_lab+' Dev',color=colors[m])
+    plt.plot(size_train,mae_train,'-',label=m_lab+' Train',color=colors[m])
+
+    #plt.title(features)
+
+plt.legend(fontsize=8)
 plt.ylim(0,1.5)
 plt.ylabel('MAE (eV)')
-plt.xlabel('% training data included')
-plt.savefig('learning_curve/learning_curve_%s.pdf'%(feature))
+plt.xlabel('# training examples included')
+plt.xlim((size_train[0],size_train[-1]))
+plt.savefig('learning_curve/learning_curve_%s.pdf'%(features))
